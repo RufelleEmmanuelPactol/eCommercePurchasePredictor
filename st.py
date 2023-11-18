e9133a0309e281ff76f7fa2b80f926e47d50928b
rufelleemmanuel.pactol <rufelleemmanuel.pactol@cit.edu>
2023-11-18 23:05:25 +0800

    added predictor

--- st.py
+++ st.py
@@ -8,83 +8,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import warnings
 
-sns.set(style="whitegrid")
-plt.rcParams.update({'font.size': 12, 'font.family': 'Arial'})
+# Set Streamlit theme to vibrant and neon
+st.set_page_config(layout="wide")
 
-
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, roc_curve, auc
-import seaborn as sns
-import warnings
-import matplotlib.pyplot as plt
-warnings.filterwarnings("ignore")
-df = pd.read_csv('archive/online_shoppers_intention.csv')
-df.head()
-# Let's identify the important features.
-# Administrative - number of pages visited
-# Administrative_Duration - time units (s) the user has been in the page
-# ProductRelated - number of viewed pages with related products
-# SpecialDay - number of days prior to a rather special day (Mother's Day, etc...)
-X = df[['Administrative', 'Administrative_Duration', 'ProductRelated', 'SpecialDay'
-    , 'ProductRelated_Duration']]
-# Revenue - Output
-y = df['Revenue']
-# Updated cache decorator
-@st.cache_data
-def load_data():
-    df = pd.read_csv('archive/online_shoppers_intention.csv')
-    X = df[['Administrative', 'Administrative_Duration', 'ProductRelated', 'SpecialDay', 'ProductRelated_Duration']]
-    y = df['Revenue']
-    return df, X, y
-
-# Let's split this data! :) Let's goo!!!
-# Initializing the model!!!! 0v0
-# Let's split this data! :) Let's goo!!!
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=55)
-model = LogisticRegression()
-model.fit(X_train, y_train)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=55)
-
-warnings.filterwarnings("ignore")
-
-# Load and preprocess the data
-@st.cache
-def load_data():
-    df = pd.read_csv('archive/online_shoppers_intention.csv')
-    X = df[['Administrative', 'Administrative_Duration', 'ProductRelated', 'SpecialDay', 'ProductRelated_Duration']]
-    y = df['Revenue']
-    return df, X, y
-
-df, X, y = load_data()
-
-# Sidebar for user input
-st.sidebar.header("Model Parameters")
-test_size = st.sidebar.slider("Test Size", 0.1, 0.3, 0.15)
-
-# Train the model
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=55)
-model = LogisticRegression()
-model.fit(X_train, y_train)
-y_hat = model.predict(X_test)
-score = accuracy_score(y_test, y_hat)
-y_probs = model.predict_proba(X_test)[:, 1]
-fpr, tpr, thresholds = roc_curve(y_test, y_probs)
-roc_auc = auc(fpr, tpr)
-import streamlit as st
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, roc_curve, auc
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
-import warnings
-
-warnings.filterwarnings("ignore")
+# Set a custom color palette
+custom_palette = sns.color_palette(["#FF5733", "#FFC300", "#DAF7A6", "#C70039", "#900C3F"])
 
 # Load and preprocess the data
 @st.cache
@@ -101,13 +29,14 @@ st.sidebar.header("Model Parameters")
 test_size = st.sidebar.slider("Test Size", 0.1, 0.3, 0.15, key='test_size_slider')
 
 # Display the dataframe
-st.write("Data Preview:", df.head())
-
+st.subheader("ECommerce Prediction With Logistic Regression: Sale or no sale?")
+st.write("Please be patient as the data is computationally intensive. Graphs may take a while to load longer than usual.")
 # EDA
 st.subheader("Exploratory Data Analysis")
 independents = ['Administrative', 'Administrative_Duration', 'ProductRelated', 'SpecialDay']
 eda = df[independents + ['Revenue']]
-fig_eda = sns.pairplot(eda, hue='Revenue').fig
+sns.set(style="whitegrid")  # Set Seaborn style
+fig_eda = sns.pairplot(eda, hue='Revenue', palette=custom_palette).fig
 st.pyplot(fig_eda)
 
 # Train the model
@@ -140,7 +69,7 @@ if st.button('Predict'):
 # ROC Curve
 st.subheader("ROC Curve")
 fig, ax = plt.subplots()
-ax.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+ax.plot(fpr, tpr, color='#FF5733', lw=3, label='ROC curve (area = %0.2f)' % roc_auc)  # Neon orange
 ax.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
 ax.set_xlabel('False Positive Rate')
 ax.set_ylabel('True Positive Rate')
@@ -148,31 +77,5 @@ ax.set_title('Receiver Operating Characteristic')
 ax.legend(loc="lower right")
 st.pyplot(fig)
 
-# Accuracy
-st.metric("Accuracy Score", score)
-
 # S-Curve Visualization
 st.subheader("S-Curve Visualization")
-plt.figure(figsize=(8, 6))
-plt.scatter(y_probs, y_test, alpha=0.2)
-plt.axhline(y=0.5, color='red', linestyle='--')
-plt.xlabel('Predicted Probability')
-plt.ylabel('Actual Outcome')
-plt.title('Logistic Regression S-Curve Visualization')
-st.pyplot(plt)
-
-# Probability Threshold Visualization
-st.subheader("Probability Threshold Visualization")
-optimal_idx = np.argmax(tpr - fpr)
-optimal_threshold = thresholds[optimal_idx]
-states = np.where(y_probs > optimal_threshold, 'True', 'False')
-fig, ax = plt.subplots(figsize=(10, 6))
-colors = {'True': 'blue', 'False': 'red'}
-for state in np.unique(states):
-    mask = states == state
-    ax.scatter(np.arange(len(y_probs))[mask], y_probs[mask], color=colors[state], label=f'{state} (Threshold = {optimal_threshold:.2f})', alpha=0.3)
-ax.set_xlabel('Sorted Index')
-ax.set_ylabel('Predicted Probability')
-ax.set_title('Logistic Regression Probability Threshold Visualization')
-ax.legend()
-st.pyplot(fig)
