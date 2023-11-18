019f7ec38de569e87e77d68d870444ed041d5e34
rufelleemmanuel.pactol <rufelleemmanuel.pactol@cit.edu>
2023-11-18 23:19:52 +0800

    Fix

--- st.py
+++ st.py
@@ -29,8 +29,11 @@ st.sidebar.header("Model Parameters")
 test_size = st.sidebar.slider("Test Size", 0.1, 0.3, 0.15, key='test_size_slider')
 
 # Display the dataframe
-st.subheader("ECommerce Prediction With Logistic Regression: Sale or no sale?")
+st.subheader("ECommerce Prediction With Logistic Regression")
+
 st.write("Please be patient as the data is computationally intensive. Graphs may take a while to load longer than usual.")
+if (st.button('Visit Documentation')):
+
 # EDA
 st.subheader("Exploratory Data Analysis")
 independents = ['Administrative', 'Administrative_Duration', 'ProductRelated', 'SpecialDay']
@@ -77,5 +80,49 @@ ax.set_title('Receiver Operating Characteristic')
 ax.legend(loc="lower right")
 st.pyplot(fig)
 
-# S-Curve Visualization
 st.subheader("S-Curve Visualization")
+
+# Define percentiles for outlier removal
+lower_percentile = 5
+upper_percentile = 95
+
+# Calculate percentile values
+lower_bound = np.percentile(y_probs, lower_percentile)
+upper_bound = np.percentile(y_probs, upper_percentile)
+
+# Create a mask to filter out outliers
+mask = (y_probs >= lower_bound) & (y_probs <= upper_bound)
+
+# Apply mask to probabilities and actual outcomes
+filtered_probs = y_probs[mask]
+filtered_actuals = y_test.to_numpy()[mask]
+
+# Plotting
+fig_s_curve, ax_s_curve = plt.subplots(figsize=(8, 6))
+ax_s_curve.scatter(filtered_probs, filtered_actuals, alpha=0.2)
+ax_s_curve.axhline(y=0.15, color='red', linestyle='--')
+ax_s_curve.set_xlabel('Predicted Probability')
+ax_s_curve.set_ylabel('Actual Outcome')
+ax_s_curve.set_title('Logistic Regression S-Curve Visualization (Outliers Removed)')
+st.pyplot(fig_s_curve)
+
+# Find the optimal threshold (closest to top-left corner of ROC curve)
+optimal_idx = np.argmax(true_positive - false_positive)
+optimal_threshold = thresholds[optimal_idx]
+
+# Apply the threshold to determine the state
+states = np.where(y_probs > optimal_threshold, 'True', 'False')
+
+# Visualize the result
+fig_threshold_visualization, ax_threshold_visualization = plt.subplots(figsize=(10, 6))
+for state in np.unique(states):
+    mask = states == state
+    ax_threshold_visualization.scatter(np.arange(len(y_probs))[mask], y_probs[mask],
+                                       color=colors[state], label=f'{state} (Threshold = {optimal_threshold:.2f})', alpha=0.3)
+
+ax_threshold_visualization.set_xlabel('Sorted Index')
+ax_threshold_visualization.set_ylabel('Predicted Probability')
+ax_threshold_visualization.set_title('Logistic Regression Probability Threshold Visualization')
+ax_threshold_visualization.legend()
+st.pyplot(fig_threshold_visualization)
+
