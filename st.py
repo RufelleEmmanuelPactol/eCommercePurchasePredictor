f46062ea7796b7dee614425b0a3ad416612ee4e4
rufelleemmanuel.pactol <rufelleemmanuel.pactol@cit.edu>
2023-11-18 22:41:04 +0800

    added predictor

--- st.py
+++ st.py
@@ -8,6 +8,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 import warnings
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, roc_curve, auc
+import seaborn as sns
+import warnings
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+df = pd.read_csv('archive/online_shoppers_intention.csv')
+df.head()
+# Let's identify the important features.
+# Administrative - number of pages visited
+# Administrative_Duration - time units (s) the user has been in the page
+# ProductRelated - number of viewed pages with related products
+# SpecialDay - number of days prior to a rather special day (Mother's Day, etc...)
+X = df[['Administrative', 'Administrative_Duration', 'ProductRelated', 'SpecialDay'
+    , 'ProductRelated_Duration']]
+# Revenue - Output
+y = df['Revenue']
+# Let's split this data! :) Let's goo!!!
+# Initializing the model!!!! 0v0
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=55)
+
 warnings.filterwarnings("ignore")
 
 # Load and preprocess the data
@@ -43,6 +70,79 @@ score = accuracy_score(y_test, y_hat)
 y_probs = model.predict_proba(X_test)[:, 1]
 fpr, tpr, thresholds = roc_curve(y_test, y_probs)
 roc_auc = auc(fpr, tpr)
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, roc_curve, auc
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# Load and preprocess the data
+@st.cache
+def load_data():
+    df = pd.read_csv('archive/online_shoppers_intention.csv')
+    X = df[['Administrative', 'Administrative_Duration', 'ProductRelated', 'SpecialDay', 'ProductRelated_Duration']]
+    y = df['Revenue']
+    return df, X, y
+
+df, X, y = load_data()
+
+# Sidebar for user input
+st.sidebar.header("Model Parameters")
+test_size = st.sidebar.slider("Test Size", 0.1, 0.3, 0.15)
+
+# Display the dataframe
+st.write("Data Preview:", df.head())
+
+# EDA
+st.subheader("Exploratory Data Analysis")
+independents = ['Administrative', 'Administrative_Duration', 'ProductRelated', 'SpecialDay']
+eda = df[independents + ['Revenue']]
+fig_eda = sns.pairplot(eda, hue='Revenue').fig
+st.pyplot(fig_eda)
+
+# Train the model
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=55)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+y_hat = model.predict(X_test)
+score = accuracy_score(y_test, y_hat)
+y_probs = model.predict_proba(X_test)[:, 1]
+fpr, tpr, thresholds = roc_curve(y_test, y_probs)
+roc_auc = auc(fpr, tpr)
+
+# ROC Curve
+st.subheader("ROC Curve")
+fig, ax = plt.subplots()
+ax.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+ax.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+ax.set_xlabel('False Positive Rate')
+ax.set_ylabel('True Positive Rate')
+ax.set_title('Receiver Operating Characteristic')
+ax.legend(loc="lower right")
+st.pyplot(fig)
+
+# Accuracy
+st.metric("Accuracy Score", score)
+
+# User input for prediction
+st.subheader("Make a Prediction")
+admin = st.number_input("Administrative (number of pages visited)", min_value=0)
+admin_duration = st.number_input("Administrative Duration (time in seconds)", min_value=0.0, format="%.2f")
+product_related = st.number_input("ProductRelated (number of viewed pages with related products)", min_value=0)
+special_day = st.number_input("SpecialDay (number of days prior to a special day)", min_value=0.0, format="%.2f")
+product_related_duration = st.number_input("ProductRelated Duration (time in seconds)", min_value=0.0, format="%.2f")
+
+if st.button('Predict'):
+    prediction = model.predict([[admin, admin_duration, product_related, special_day, product_related_duration]])
+    prediction_proba = model.predict_proba([[admin, admin_duration, product_related, special_day, product_related_duration]])
+    st.write(f"Prediction: {'Revenue Generation Likely' if prediction[0] else 'Revenue Generation Unlikely'}")
+    st.write(f"Prediction Probability: {prediction_proba[0][1]:.2f}")
 
 # ROC Curve
 st.subheader("ROC Curve")
